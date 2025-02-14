5f2a5cd0ac4038de95ace20546474fe2004d287d
jslblar080 <jslblar080@gmail.com>
2025-02-14 15:05:23 +0900

    정적 함수 사용

--- __main__.py
+++ __main__.py
@@ -1,4 +1,4 @@
 from config.chapter_config import ChapterConfig
 
 if __name__ == "__main__":
-    ChapterConfig().chapter().play()
+    ChapterConfig.chapter().play()
--- config/chapter_config.py
+++ config/chapter_config.py
@@ -4,9 +4,6 @@ from chapter.learn_series import LearnSeries
 
 class ChapterConfig:
 
-    def __init__(self):
-        self.chapter()
-
     @staticmethod
     def chapter() -> BaseChapter:
         return LearnSeries()
